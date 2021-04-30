08fad0a6c712b795f02d86b3897872b39811e376
sravittxof <travis.fox90@gmail.come>
2021-04-29 23:03:03 -0400

    commit tree c87a4c0d97d4c4c4edb6f345a0a313634ba0b3c5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(sourceArray, callbackFunction){
+    let newArray = [];
+    for (const elem of sourceArray) {
+        newArray.push(callbackFunction(elem));
+    }
+    return newArray;
+}
+
+function reduce(sourceArray, reducer, init){
+    let accum;
+    let workingSourceArray = [...sourceArray];
+    if (!!init === false){
+        accum = workingSourceArray.shift();
+    } else {
+        accum = init;
+    }
+    for (const elem of workingSourceArray){
+        accum = reducer(accum, elem);
+    }
+    return accum;
+}
